443fa8d1a15da5e67c43c3986f5bce63ff8ca104
Mohamed Chérif Diallo <diallo.mohamed-cherif@ugb.edu.sn>
2024-02-22 18:32:56 +0100

    Ajout d'un nouveau controller pour renvoyer une liste d'utilisateurs

--- src/main/java/sn/uasz/cicd/controller/HelloCicdController.java
+++ src/main/java/sn/uasz/cicd/controller/HelloCicdController.java
@@ -4,8 +4,11 @@
  */
 package sn.uasz.cicd.controller;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sn.uasz.cicd.records.User;
 
@@ -22,8 +25,16 @@ public class HelloCicdController {
         return "Hello Spring Boot CICD ";
     }
     
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id, @RequestParam String prenom, @RequestParam String nom){
+        return new User(id, prenom, nom);
+    }
+    
     @GetMapping("/users")
-    public User getUsers(){
-        return new User(1l, "Mohamed", "Diallo");
+    public List<User> getUser(){
+        User user1 = new User(1l, "Alpha", "Diop");
+        User user2 = new User(2l, "Modou", "Fall");
+        User user3 = new User(3l, "Sana", "Badji");
+        return List.of(user1,user2,user3);
     }
 }
